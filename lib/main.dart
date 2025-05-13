c23e18d5c97e437db8833b47ccdd2ace091c16b3
Yanuar Eka <pramudyanuar@gmail.com>
2025-05-14 00:38:23 +0700

    feat: tf 6 finish

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           title: 'Wanigo Nasabah',
           debugShowCheckedModeBanner: false,
           theme: AppTheme.lightTheme,
-          initialRoute: Routes.laporanSampah,
+          initialRoute: Routes.splash,
           getPages: AppPages.routes,
           defaultTransition: Transition.fadeIn,
           transitionDuration: const Duration(milliseconds: 300),
